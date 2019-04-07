35fed4fe6d48c1cb9ccd8e5b8e24f28ff51cfa18
Carlos Cuello <cjcuello19@gmail.com>
2019-04-06 22:05:37 -0400

    For Loops Session

--- scratch.py
+++ scratch.py
@@ -1,8 +1,22 @@
-dog_breeds_available_dor_adoption = [ 'french_bulldogs', 'dalmation', 'shihtzu', 'poodle', 'collie']
+dog_breeds_available_for_adoption = [ 'french_bulldogs', 'German', 'Dalmation', 'poodle', 'collie']
 dog_breed_I_want = 'dalmation'
 
-for dog_breed in dog_breeds_available_dor_adoption:
+for dog_breed in dog_breeds_available_for_adoption:
    print(dog_breed)
    if dog_breed == dog_breed_I_want:
       print("They have the dog I want!")
       break
+
+
+board_games = ["Monopoly", "Dominoes", "Chess", "Parcheesi"]
+
+sport_games = ["football", "baseball", "Tennis", "Track"]
+
+for game in board_games:
+    print(game)
+
+for game in sport_games:
+    print(game)
+
+
+
