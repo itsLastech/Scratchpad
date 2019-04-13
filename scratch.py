169a1d7b4848a55e773f751d4ef80433be9751c1
Carlos Cuello <cjcuello19@gmail.com>
2019-04-13 14:34:12 -0400

    HELLO PYTHON PPL WHO FOUND ME I LIKE SNAKES IM 3 LOL

--- scratch.py
+++ scratch.py
@@ -34,7 +34,7 @@ def math_problemo(numbers):
 math_problemo(1)
 math_problemo(2)
 math_problemo(3)
-math_problemo(4)'''
+math_problemo(4)
 
 
 
@@ -50,4 +50,12 @@ def math_problem(number, X, Y):
    
 
 math_problem(5, 2, 3)
-math_problem(1, 6, 22)
+math_problem(1, 6, 22)'''
+
+def spreadsheet(title, row_count = 1000):
+   print('create a spreadsheet called '+title+' with '+str(row_count)+' row.')
+
+spreadsheet("Hi")
+spreadsheet("Applications", 100)
+
+
